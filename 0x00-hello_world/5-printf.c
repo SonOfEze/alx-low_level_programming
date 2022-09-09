c17a8e31906a6dfc0bfb9d6a5926b7e3efeccda0
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-09 11:02:18 -0700

    C Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *   main - Print the string in the put function
- *   
- *   Description Using the main function
- *   Program to print with proper grammar, but the outcome is a piece of art,
- *   Return 0  
+ * main - Print the string in the put function
+ * Description Using the main function
+ * Program to print with proper grammar, but the outcome is a piece of art,
+ * Return 0  
  */
 int main(void)
 {
