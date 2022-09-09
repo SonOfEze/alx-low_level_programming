4fa1109328fdf76770dc1a6da09e4a2f8efc787e
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-09 10:41:11 -0700

    C Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  *   Program to print with proper grammar, but the outcome is a piece of art,  
  */
 int main(void)
-}
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
