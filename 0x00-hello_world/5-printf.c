d05a1751a7fd41865808fa2669bb430472392ab4
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-09 10:47:35 -0700

    C Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  *   main - Print the string in the put function
  *   
  *   Description Using the main function
- *   Program to print with proper grammar, but the outcome is a piece of art,  
+ *   Program to print with proper grammar, but the outcome is a piece of art,
+ *   Return 0  
  */
 int main(void)
 {
