475ed4d34cc119006c6a15c7968ec6a0b07ee1a9
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-12 14:34:07 -0700

    0x01 C Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ * starting from 0 as first number, and then a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i=0; i < 10; i++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
