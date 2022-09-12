29dbda78260fb9a8c100844e6fb862a7c5da1155
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-12 12:03:36 -0700

    C 0x01 Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *   main - prints all possible combinations
+ *   To always Return: 0
+ */   
+
+int main(void)
+{
+
+   int n, m;
+
+   for (n =20; n <= 35; n++)
+   {
+   	for (m=30; m <=40 m++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+
+			else if (n != 35 || m != 40)
+			{
+				putchar (',');
+				putchar (',');
+			}
+		}
+	}
+   }
+   putchar('\n');
+   return (0);
+
+}
