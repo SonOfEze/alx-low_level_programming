780f4af87338553c136fa38c008f90bdf0597a88
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-12 17:17:52 -0700

    0x01 C Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,12 @@ int main(void)
 	{
 		for (num_b = num_a + 1; num_b < 10; num_b++)
 		{
-			putchar(a);
-			putchar(b);
+			putchar(num_a);
+			putchar(num_b);
 			if (a != 56 || b != 57)
 			{
 				putchar((num_a % 10) + '0');
-				putchar(' num_b % 10) + '0');
+				putchar((num_b % 10) + '0');
 				
 				if (num_a == 8 && num_b == 9)
 
