0b7b9f90b791541d08157e204ce34df27bb9fd82
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-15 07:29:10 -0700

    0x02 C  Task 0

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x02 Task 0 _putchar
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+/**
+ *main - check the code
+ *Write a function that prints the alphabet, in lowercase, followed by a new line.
+ *Return: Always 0.
+ */
+
+
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int low (int l);
+int alpha(int alph);
+int printSign(int pSign);
+int abc(int );
+int print_last_digit(int);
+void john_Doe(void);
+void time_table(void);
+int add(int a, int b);
+void print_to_98(void);
+void print_time_table(void);
+void num(int c, int d);
+
+#endif
