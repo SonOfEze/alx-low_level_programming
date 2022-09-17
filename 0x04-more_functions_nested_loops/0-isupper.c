450d4bec87e564910b21f0288b9120fed804fa7f
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-16 21:39:50 -0700

    0x04 C Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _isupper - check the code.
+ * _isupper int c - check the code.
  * @c character to be tested.
  *Return: Always 0.
  */
 
 int _isupper(int c)
 {
-	if ((c>= 'A') && (c <= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
