05afae331831e4f8c9ec7581ce9dd66648accbca
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-16 22:18:47 -0700

    0x04 C Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *Return: Always 0.
+ */
+
+int -isdigit(int c)
+{
+	if ((C >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+	return (0);
+}
