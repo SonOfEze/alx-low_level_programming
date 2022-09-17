6cbbfa673f22b3c13898bcfc75dde3c07b3c7d51
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-16 22:47:23 -0700

    0x04 Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - multiply 2 integers
+*@a: first int
+*@b: second int
+*Return: total of plutiplication
+*/
+
+int mul(int a int b)
+{
+	return (a * b)
+	_putchar('\n');
+}
