18c556078160f37039df642e7038574ec8d4d788
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-16 21:34:51 -0700

    0x04 C Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *main - header file for prototypes
+ *void prototypw
+ *int prototype
+ */
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(void);
+void print_diagonal(int i);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int i);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int nul(int, int);
+
+#endif /* MAIN_H */
