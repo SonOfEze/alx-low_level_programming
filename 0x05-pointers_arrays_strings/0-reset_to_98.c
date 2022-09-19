d4a1c10f48ea26782b6e09000a877cd6d1aa8c50
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-19 16:00:32 -0700

    0x05 Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * rset_to_98 - reset the value of int
+ * reset_to_98 - reset the value of int
  * @n:hold initial value
  * Return: 0
  */
 
 void reset_to_98(int *n)
 {
-	*n =m 98
+	*n = 98
 }
