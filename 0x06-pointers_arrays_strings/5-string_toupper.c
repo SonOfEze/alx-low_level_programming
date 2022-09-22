78bb4c56a4a1a6ea0060111a15a173464523ce6c
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 03:21:20 -0700

    0x06 C Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters to uppercase
+ * @str: string to be changed
+ * Return: location of the string
+ */
+
+char *string_toupper(char *)
+{
+	int i - 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
