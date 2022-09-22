7c4676d204a38cceb94d7b37bebc0a2bcb707c60
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 08:29:00 -0700

    0x01 C Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to add 3 numbers
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int n1 = 0; n2, n3;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <= 9)
+		{
+			n3 = 9;
+			while (n3 <= 9)
+			{
+				if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+				{
+					putchar(n1 + 48);
+					putchar(n2 + 48);
+					putchar(n3 + 48);
+
+					if (n1 + n2 + n3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				++n3
+			}
+			++n2
+		}
+		++n1
+	}
+	putchar('\n');
+	return (0);
+}
