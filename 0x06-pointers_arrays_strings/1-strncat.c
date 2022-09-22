0fe9aa8f268b7a8d1d13fd67edbc0de7be8442e4
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 00:44:31 -0700

    0x06 C Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - contcatenates string pointed by src
+ * @dest: String to be appended
+ * @src: String to be concatenated
+ * @n: integer parameter to compare index
+ * Return: returns pointer to @dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++ = src[index];
+
+	return (dest);
+}
