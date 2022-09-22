5e570d841eff044c14e9de73211188fc22608e7e
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 11:44:10 -0700

    0x03 C Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,6 @@ void positive_or_negative(int i);
 int largest_num(int, int, int);
 void print_days_left(int, int, int);
 int days_cinvert(int, int);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
