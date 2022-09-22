c8c03a3424bac5bdc561ff3c81163f182a09d3e1
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 05:18:08 -0700

    0x06 C Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
-void print_number(int n)
-{int n;
+int main (void)
+{
+int n;
 int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
 
-print_number(98);
-_putchar('\n');
-print_number(402);
-_putchar('\n');
-print_number(1024);
-_putchar('\n');
-print_number(0);
-_putchar('\n');
- print_number(-98);
-_putchar('\n');
+printf("a[2] = %d\n", a[2]);
 return (0);
-
 /**
  *
  *
