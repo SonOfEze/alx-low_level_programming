d67e60981cd9d2c572ac711bc659e9b3f58dc431
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 05:07:35 -0700

    0x06 C Task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+void print_number(int n)
+{int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+
+print_number(98);
+_putchar('\n');
+print_number(402);
+_putchar('\n');
+print_number(1024);
+_putchar('\n');
+print_number(0);
+_putchar('\n');
+ print_number(-98);
+_putchar('\n');
+return (0);
+
+/**
+ *
+ *
+ */
+ }
