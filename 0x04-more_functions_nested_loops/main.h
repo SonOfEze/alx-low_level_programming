5ea99bbd0386a31697b1ca5747bc33955a62e42f
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 19:02:11 -0700

    0x04 Task 1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,7 +17,7 @@ void print_triangle(int size);
 void print_number(int i);
 int _putchar(char);
 int _isupper(int);
-int _isdigit(int);
+int _isdigit(int c);
 int nul(int, int);
 
 #endif /* MAIN_H */
