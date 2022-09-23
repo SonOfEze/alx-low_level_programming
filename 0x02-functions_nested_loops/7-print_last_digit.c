dba4a5c404aa19c1516a5083fa6debbc290e3f68
SonOfEze <patrick.nkwocha@outlook.com>
2022-09-22 17:03:33 -0700

    0x02 C Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,11 @@
 
 int print_last_digit(int n)
 {
-	int x;
-
+	n %= 10;
 	if (n < 0)
-	n = -n;
-
-	x = n % 10;
-
-	_putchar(x + '0');
-
-	return (x);
+	{
+		n *= -1;
+	}
+	_putchar('0' + n);
+	return (n);
 }
